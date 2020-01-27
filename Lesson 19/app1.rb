72faa4f476e4a8826cdbf726c47cf8eedf259b93
alexnvb <alexgridin@mail.ru>
2020-01-27 23:24:08 +0700

    19

--- Lesson 19/app1.rb
+++ Lesson 19/app1.rb
@@ -1,9 +1,17 @@
 
 require 'sinatra'
+
 get '/' do
   erb :index
 end
 
 post '/' do
-  erb :index
+  @login = params[:aaa]
+  @password = params[:bbb]
+
+  if @login == 'admin' && @password == 'secret'
+    erb :welcome
+  else @login != 'admin'&&@password != 'secret'
+    erb :denied
+  end
 end
