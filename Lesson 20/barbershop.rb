d53fda1b5911dd414c05ac6b61e018e3d4ff0a2a
alexnvb <48714129+alexnvb@users.noreply.github.com>
2020-02-03 20:47:32 +0700

    20

--- Lesson 20/barbershop.rb
+++ Lesson 20/barbershop.rb
@@ -9,6 +9,8 @@ post '/' do
   @user_name=params[:user_name]
   @phone=params[:phone]
   @date_time=params[:date_time]
+  @login=params[:login]
+  @password=params[:password]
 
   @title = "Thank you"
   @message="Dear #{@user_name}, we waiting you at #{@date_time}"
@@ -16,6 +18,11 @@ post '/' do
   f = File.open 'users.txt', 'a'
   f.write "User: #{@user_name} Phone: #{@phone} Date: #{@date_time}"
   f.close
+
+  if @login=="admin" && @password=="admin"
+    erb :welcome
+  end
   
   erb :message
+
 end
