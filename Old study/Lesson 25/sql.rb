a8b8f1712dee2f27ebd67cf68b4376b8d0753fc5
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2020-05-27 11:13:21 +0700

    db SELECT

--- Old study/Lesson 25/sql.rb
+++ Old study/Lesson 25/sql.rb
@@ -3,6 +3,9 @@ require 'sqlite3'
 
 db=SQLite3::Database.new 'test.db'
 
-db.execute"INSERT INTO Cars (Name, Price) VALUES ('Jaguar', '777')"
+#db.execute"INSERT INTO Cars (Name, Price) VALUES ('Jaguar', '777')"
 
-db.close
+db.execute"SELECT * FROM Cars" do |car|
+  p car
+  p "++++"
+end
